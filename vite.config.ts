7638d94deb0d89bcee5d8e9f05b16d4dc8d69bc5
hermosamotif-stack <hermosamotif@gmail.com>
2026-03-26 17:15:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // ഇതാണ് നിങ്ങൾ പുതുതായി ചേർക്കേണ്ട വരി
   server: {
     host: "::",
     port: 8080,
